03100fa47bd46509dd6da5055562bd502a679c26
Flavia <flaviaycaballero@gmail.com>
2025-05-27 21:17:24 -0300

    ultimos cambios formularios Flavia 27-05

---
+++ JavaScript/altaSalon.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () =>{
+
+// Verificar si el usuario está logueado:
+if(sessionStorage.getItem("usuario")) {
+    alert("Debe loguearse");
+    window.location.href = "login.html";
+    return;
+}
+
+const salir = document.getElementById("logout");
+    if (salir) {
+        salir.addEventListener("click", () => {
+            sessionStorage.clear();
+            window.location.href = "login.html";
+       });
+    };
+
+// Para manejar el formulario de alta de salón:
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    const nombre = document.getElementById("nombreSalon").value;
+    const direccion = document.getElementById("direccion").value;
+    const capacidad = document.getElementById("capacidad").value;
+
+    alert (`El nuevo salón tiene los siguientes atributos nombre: ${nombre}, dirección: ${direccion}, capacidad: ${capacidad}`);
+    
+    this.reset();
+    });
+});
